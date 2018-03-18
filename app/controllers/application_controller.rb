702763683be36d05b972b563d1044812457ffeaa
royne <royne.chaves@gmail.com>
2018-03-18 01:03:00 -0500

    se crea perfiles de usuarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
+  get 'profiles/show'
+  get '/:id', to: 'profiles#show'
+
   resources :posts
 
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
