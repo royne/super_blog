5a17351db1ae1136dbb3d9c725baaf99881d4f80
royne <royne.chaves@gmail.com>
2018-03-19 22:59:46 -0500

    implementa las imagenes para posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318192558) do
+ActiveRecord::Schema.define(version: 20180320005155) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20180318192558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
